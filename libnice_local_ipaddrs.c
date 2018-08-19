e585052ee7356f0aac5e3fe201c4b819ce6d0d4a
Ilya Evseev <ilya.evseev@gmail.com>
2018-08-20 01:51:10 +0300

    libnice_local_ipaddrs: dbgprint added.

--- libnice_local_ipaddrs.c
+++ libnice_local_ipaddrs.c
@@ -1,8 +1,15 @@
-/* print_local_ipaddrs.c */
+/* libnice_local_ipaddrs.c */
 
 #include <nice/interfaces.h>
-#include <string.h>  /* strchr */
+#include <string.h>  /* strchr, strlen */
 #include <stdlib.h>  /* getenv */
+#include <unistd.h>  /* write */
+
+static void dbgprint(const char *s)
+{
+	if (getenv("LIBNICE_LOCAL_IPADDRS_DEBUG"))
+		write(1, s, strlen(s));
+}
 
 GList *nice_interfaces_get_local_ips(gboolean include_loopback)
 {
@@ -11,8 +18,12 @@ GList *nice_interfaces_get_local_ips(gboolean include_loopback)
 	const char *q;
 	GList *result = NULL;
 
-	if (!s)
+	if (!s) {
+		dbgprint("DEBUG: libnice_local_ipaddrs: return empty list\n");
 		return g_list_append(NULL, g_strdup("127.0.0.1"));
+	}
+
+	dbgprint("DEBUG: libnice_local_ipaddrs: parse envvar\n");
 
 	while((q = strchr(p, ':')) != NULL) {
 		result = g_list_append(result, g_strndup(p, q - p));
